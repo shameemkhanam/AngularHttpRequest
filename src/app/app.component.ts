c657e44f2e9afca535aafa77a9c596f7cea370f1
unknown <shameemkhanam@gmail.com>
2023-01-06 23:22:06 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularHttpRequest';
+
+  constructor(private http: HttpClient){}
+
+  onProductCreate(products: {pName: string, desc: string, price: string}) {
+    console.log(products);
+    const headerProp = new HttpHeaders({ 'myHeader': 'procademy' });
+    this.http
+      .post(
+        'https://angularproject-77e34-default-rtdb.firebaseio.com/products.json',
+        products,
+        { headers: headerProp } //passing our custom header ie. headerProp with the request
+      )
+      .subscribe((res) => {
+        console.log(res);
+      });
+  }
 }
